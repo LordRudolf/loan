5da9a5438c6bd4ad4c72fafa4c72bb09dc7fd4da
LordRudolf <35683078+LordRudolf@users.noreply.github.com>
2023-11-21 00:15:42 +0200

    Helper function for auto-detecting which variables are available

--- R/utils.R
+++ R/utils.R
@@ -39,11 +39,25 @@ parse_attributes_df <- function(df, col) {
   attributes_df <- tibble(col = df)
 }
 
-discretize_values <- function(var, breaks) {
-  #var <- as.character(infotheo::discretize(var, nbins = 10)[[1]])
-  var <- arules::discretize(var, breaks = breaks, infinity = TRUE)
-  levels(var) <- c(levels(var), 'value_NA')
-  var[is.na(var)] <- 'value_NA'
+discretize_values <- function(var, breaks, discretized_intervals = NULL) {
+
+  if(!is.null(discretized_intervals)) {
+    discretized_intervals <- as.character(discretized_intervals)
+    new_var <- rep(NA, length = length(var))
+
+    int_from <- stringr::str_extract(discretized_intervals[[i]], "-Inf|\\d+\\.*\\d*")
+    for(i in 2:length(discretized_intervals)) {
+      int_to <- stringr::str_extract(discretized_intervals[[i]], "-Inf|\\d+\\.*\\d*")
+      new_var[var >= int_from & var < int_to & !is.na(var)] <- discretized_intervals[[i-1]]
+      int_from <- int_to
+    }
+    new_var[is.na(var)] <- 'value_NA'
+    var <- as.factor(new_var, levels = discretized_intervals)
+  } else {
+    var <- arules::discretize(var, breaks = breaks, infinity = TRUE)
+    levels(var) <- c(levels(var), 'value_NA')
+  }
+
   return(var)
 }
 
@@ -152,3 +166,69 @@ detect_application_status <- function(application_status_var) {
   ## TO DO: all of this
   return(application_status)
 }
+
+
+error_message <- function(vec) {
+  stop(paste0('The argument ',
+              vec,
+              ', if defined by a user, must be a vector with length equal to the number of rows in `df`, or a character length == 1 corresponding to column names in `df`'))
+}
+  
+variables_availability_check <- function(df, required_vectors, ..., optional_vectors = NULL) {
+  # Extract additional arguments
+  args <- modifyList(list(...), as.list(parent.frame()), keep.null = TRUE)
+  if(length(optional_vectors) >= 0) required_vectors <- unique(c(required_vectors, optional_vectors))
+  
+  args <- args[names(args) %in% required_vectors]
+  
+  for(vec in names(args)) {
+    if(is.null(args[[vec]])) {
+      args[[vec]] <- NULL
+    } else if (length(args[[vec]]) == 1 && is.na(args[[vec]])) {
+      args[[vec]] <- NULL
+    }
+  }
+  
+  # Initialize a list to store the final variables
+  final_vars <- list()
+  
+  # Loop through each required vector
+  for (vec in required_vectors) {
+    
+    if (vec %in% names(args)) {
+      if(length(args[[vec]]) == 1) { #The case when the vector can be found in `df` by column name
+        if(!is.character(args[[vec]])) error_message(vec)
+        the_var <- df[, args[[vec]] ]
+        if(is.null(the_var)) stop(paste0('The variable ', vec, ' has not been found in `df`'))
+        final_vars[[vec]] <- the_var
+      } else if(length(args[[vec]]) == nrow(df)) {
+        final_vars[[vec]] <- args[[vec]]
+      } else {
+        error_message(vec)
+      }
+      
+      #End of args[[vec]]) == 1
+      
+    } else if(class(df)[1] == 'loan_df') {
+      if(any(vec == names(attributes(df)$aliases))) {
+        
+        final_vars[[vec]] <- attributes(df)$aliases[[vec]]
+        
+      } else {
+        stop(sprintf("The column `%s` hasn't been defined.", vec))
+      }
+
+      
+    } else if (vec %in% optional_vectors) {
+      
+      ## Do nothing. The optional variable hasn't been found. Continue the loop
+      
+    } else {
+      # If the vector is neither provided as an argument nor a column in df, throw an error
+      stop(sprintf("The column `%s` hasn't been defined.", vec))
+    }
+  }
+  
+  return(final_vars)
+}
+
